5e7e1244ba8ff6fe8a12dbfdbb32d883cec442aa
manaswini <manaswini0623@gmail.com>
2024-06-11 18:20:46 +0530

    connected MongoDB with backend using mongoose

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000
 
+// Connecting the dbConnect.js file 
+const connectDB = require('./connectDB');
+
 // Middleware for parsing JSON request body
 app.use(express.json());
 
@@ -10,7 +13,18 @@ app.get("/ping", (req, res) => {
     res.json({"message": "pong"});
 });
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-});
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    // If connecting is successful, then we do app.listen
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+module.exports = app;
 
